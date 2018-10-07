730d31ab35d02295768107cb63a0482f4e158239
mats <123@abc.no>
2018-10-07 17:35:24 +0200

    added semicolons

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const body_parser = require('body-parser');
 const express = require('express');
 const helmet = require('helmet');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const allow_cross_domain = require('./app/allow-cross-domain');
 const message_route = require('./app/routes/message.route.js');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(helmet())
-app.use(morgan('combined'))
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
 app.use(allow_cross_domain);
 app.use('/api/messages', message_route);
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+    console.log(`Server listening on port ${port}`);
+});
