7eeac8bfe4da25558c70a61f9767975acbb97f97
mats <123@abc.no>
2018-10-14 21:39:19 +0200

    import cors

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const allow_cross_domain = require('./app/allow-cross-domain');
-const message_route = require('./app/routes/message.route.js');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(helmet());
 app.use(morgan('combined'));
-app.use(body_parser.urlencoded({ extended: false }));
-app.use(body_parser.json());
-app.use(allow_cross_domain);
-app.use('/api/messages', message_route);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/messages', require('./app/routes/message.route.js'));
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
